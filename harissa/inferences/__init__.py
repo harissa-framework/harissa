f547bdd20aa1ae2db245096e414af7d09fe43bb7
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2023-10-26 17:03:31 +0200

    remove interface from init files

--- harissa/inferences/__init__.py
+++ harissa/inferences/__init__.py
@@ -4,7 +4,6 @@ harissa.inference
 
 Inference of the network model.
 """
-from harissa.inferences.inference import Inference
 from harissa.inferences.hartree.hartree import Hartree
 
-__all__ = ['Inference', 'Hartree']
+__all__ = ['Hartree']
--- harissa/simulations/__init__.py
+++ harissa/simulations/__init__.py
@@ -5,8 +5,7 @@ harissa.simulation
 Simulation of the network model.
 """
 
-from harissa.simulations.simulation import Simulation
 from harissa.simulations.bursty_pdmp.bursty_pdmp import BurstyPDMP
 from harissa.simulations.approx_ode.approx_ode import ApproxODE
 
-__all__ = ['Simulation', 'BurstyPDMP', 'ApproxODE']
+__all__ = ['BurstyPDMP', 'ApproxODE']
