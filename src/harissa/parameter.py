e65f2fe33962574d202d177591f00378079f4cd6
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2023-12-15 10:49:25 +0100

    add setters

--- src/harissa/parameter.py
+++ src/harissa/parameter.py
@@ -28,16 +28,16 @@ class NetworkParameter:
         # Number of genes
         self._n_genes = _check_n_genes(n_genes)
         # Mask for ignoring stimulus
-        G = n_genes + 1
-        m = np.zeros((G,G), dtype=bool)
-        m[:,0] = True
+        G = self.n_genes_stim
+        mask = np.zeros((G,G), dtype=bool)
+        mask[:, 0] = True
         # Initialize parameters
-        self._burst_frequency = _masked_zeros((2,G), m[:2])
-        self._burst_size_inv = _masked_zeros(G, m[0])
-        self._creation = _masked_zeros((2,G), m[:2])
-        self._degradation = _masked_zeros((2,G), m[:2])
-        self._basal = _masked_zeros(G, m[0])
-        self._interaction = _masked_zeros((G,G), m)
+        self._burst_frequency = _masked_zeros((2,G), mask[:2])
+        self._burst_size_inv = _masked_zeros(G, mask[0])
+        self._creation = _masked_zeros((2,G), mask[:2])
+        self._degradation = _masked_zeros((2,G), mask[:2])
+        self._basal = _masked_zeros(G, mask[0])
+        self._interaction = _masked_zeros((G,G), mask)
         # Default bursting parameters
         self._burst_frequency[0] = 0.0
         self._burst_frequency[1] = 2.0
@@ -46,8 +46,9 @@ class NetworkParameter:
         self._degradation[0] = np.log(2.0) / 9.0
         self._degradation[1] = np.log(2.0) / 46.0
         # Default creation rates
-        self._creation[0] = self._degradation[0] * self.scale
-        self._creation[1] = self._degradation[1] * self.scale
+        scale = self.scale
+        self._creation[0] = self._degradation[0] * scale
+        self._creation[1] = self._degradation[1] * scale
 
     def __eq__(self, other):
         if isinstance(other, NetworkParameter):
@@ -77,46 +78,93 @@ class NetworkParameter:
     def burst_frequency_min(self):
         """Minimal bursting frequency for each gene (low expression)."""
         return self._burst_frequency[0]
+    
+    @burst_frequency_min.setter
+    def burst_frequency_min(self, arr):
+        self._burst_frequency[0] = _check_array(
+            arr, 
+            (self._burst_frequency.shape[0],)
+        )
+            
 
     @property
     def burst_frequency_max(self):
         """Maximal bursting frequency for each gene (high expression)."""
         return self._burst_frequency[1]
+    
+    @burst_frequency_max.setter
+    def burst_frequency_max(self, arr):
+        self._burst_frequency[1] = _check_array(
+            arr, 
+            (self._burst_frequency.shape[1],)
+        )
 
     @property
     def burst_size_inv(self):
         """Inverse of average burst size for each gene."""
         return self._burst_size_inv
+    
+    @burst_size_inv.setter
+    def burst_size_inv(self, arr):
+        self._burst_size_inv = _check_array(
+            arr, 
+            self._burst_size_inv.shape, 
+            True
+        )
 
     @property
     def creation_rna(self):
         """mRNA creation rates."""
         return self._creation[0]
+    
+    @creation_rna.setter
+    def creation_rna(self, arr):
+        self._creation[0] = _check_array(arr, (self._creation.shape[0],))
 
     @property
     def creation_protein(self):
         """Protein creation rates."""
         return self._creation[1]
+    
+    @creation_protein.setter
+    def creation_protein(self, arr):
+        self._creation[1] = _check_array(arr, (self._creation.shape[1],))
 
     @property
     def degradation_rna(self):
         """mRNA degradation rates."""
         return self._degradation[0]
+    
+    @degradation_rna.setter
+    def degradation_rna(self, arr):
+        self._degradation[0] = _check_array(arr, (self._degradation.shape[0],))
 
     @property
     def degradation_protein(self):
         """Protein degradation rates."""
         return self._degradation[1]
+    
+    @degradation_protein.setter
+    def degradation_protein(self, arr):
+        self._degradation[1] = _check_array(arr, (self._degradation.shape[1],))
 
     @property
     def basal(self):
         """Basal expression level for each gene."""
         return self._basal
+    
+    @basal.setter
+    def basal(self, arr):
+        self._basal = _check_array(arr, self._basal.shape, True)
 
     @property
     def interaction(self):
         """Interactions between genes."""
         return self._interaction
+    
+    @interaction.setter
+    def interaction(self, arr):
+        self._interaction = _check_array(arr, self._interaction.shape, True)
 
     # Aliases (mathematical notation)
     # ===============================
@@ -125,31 +173,59 @@ class NetworkParameter:
     def k(self):
         """Bursting frequency bounds for each gene."""
         return self._burst_frequency
+    
+    @k.setter
+    def k(self, arr):
+        self._burst_frequency = _check_array(
+            arr, 
+            self._burst_frequency.shape, 
+            True
+        )
 
     @property
     def b(self):
         """Inverse of average burst size for each gene."""
-        return self._burst_size_inv
+        return self.burst_size_inv
+    
+    @b.setter
+    def b(self, arr):
+        self.burst_size_inv = arr
 
     @property
     def s(self):
         """mRNA and protein creation rates."""
         return self._creation
 
+    @s.setter
+    def s(self, arr):
+        self._creation = _check_array(arr, self._creation.shape, True)
+
     @property
     def d(self):
         """mRNA and protein degradation rates."""
         return self._degradation
+    
+    @d.setter
+    def d(self, arr):
+        self._degradation = _check_array(arr, self._degradation.shape, True)
 
     @property
     def beta(self):
         """Basal expression level for each gene."""
-        return self._basal
+        return self.basal
+    
+    @beta.setter
+    def beta(self, arr):
+        self.basal = arr
 
     @property
     def theta(self):
         """Interactions between genes."""
-        return self._interaction
+        return self.interaction
+    
+    @theta.setter
+    def theta(self, arr):
+        self.interaction = arr
 
     # Shortcut properties
     # ===================
@@ -176,8 +252,10 @@ class NetworkParameter:
         If shallow is True, underlying arrays are only referenced."""
         new_param = NetworkParameter(self._n_genes)
         for k in self._array_names():
-            if shallow: setattr(new_param, k, getattr(self, k))
-            else: getattr(new_param, k)[:] = getattr(self, k)
+            if shallow: 
+                setattr(new_param, k, getattr(self, k))
+            else: 
+                getattr(new_param, k)[:] = getattr(self, k)
         return new_param
 
 
@@ -188,10 +266,31 @@ def _check_n_genes(arg):
     if isinstance(arg, int):
         if arg > 0:
             return arg
+        else:
+            raise ValueError('n_genes should be a positive integer')
     raise TypeError('n_genes should be a positive integer')
 
+def _check_array(arr, shape, masked = False):
+    if isinstance(arr, np.ndarray):
+        if arr.dtype != np.float64:
+            raise ValueError(f'The argument of dtype {arr.dtype} ' 
+                              'must have a np.float64 dtype')
+        if arr.shape != shape:
+            raise ValueError(f'The argument of shape {arr.shape} ' 
+                            f'must have a {shape} shape')
+        if masked:
+            mask = np.zeros(arr.shape, dtype=bool)
+            if arr.ndim == 1:
+                mask[0] = True
+            else:
+                mask[:, 0] = True
+
+            arr = np.ma.array(arr, mask=mask, hard_mask=True)
+
+        return arr
+    raise TypeError('The argument must be a ndarray')
+    
+
 def _masked_zeros(shape, mask):
-    """Define a hard-masked array of zeros with given shape and mask.
-    Note that np.zeros is used instead of np.empty in order to avoid
-    a runtime warning problem with numpy.ma operations."""
+    """Define a hard-masked array of zeros with given shape and mask."""
     return np.ma.array(np.zeros(shape), mask=mask, hard_mask=True)
