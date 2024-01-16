31c191cbdfc607ef9a421aba9a3f41860d116a41
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-01-16 12:21:08 +0100

    minor changes

--- src/harissa/cli.py
+++ src/harissa/cli.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse as ap
 from pathlib import Path
 import numpy as np
 
@@ -7,6 +7,7 @@ import harissa.simulation
 import harissa.inference
 import harissa.graphics
 
+# Inferences creation
 def create_hartree(args):
     options = {'verbose' : args.verbose}
     if args.penalization is not None:
@@ -19,18 +20,11 @@ def create_hartree(args):
         options['use_numba'] = args.use_numba
     return harissa.inference.Hartree(**options)
 
-def infer(args):
-    model = harissa.NetworkModel(inference=args.create_inference(args))
-    print('inferring ...')
-    model.fit(np.loadtxt(args.data_path))
+# TODO
+def create_cardamom(args):
+    ...
 
-    inter = (np.abs(model.interaction) > args.cut_off) * model.interaction
-    
-    pos = harissa.graphics.build_pos(inter)
-    harissa.graphics.plot_network(inter, pos, scale=2)
-    print('done')
-
-
+# Simulations creation 
 def create_bursty(args):
     options = {'verbose' : args.verbose, 'use_numba': args.use_numba}
     if args.thin_adapt is not None:
@@ -42,6 +36,20 @@ def create_ode(args):
         verbose=args.verbose, 
         use_numba=args.use_numba
     )
+
+# infer command
+def infer(args):
+    model = harissa.NetworkModel(inference=args.create_inference(args))
+    print('inferring ...')
+    model.fit(np.loadtxt(args.data_path))
+
+    inter = (np.abs(model.interaction) > args.cut_off) * model.interaction
+    
+    pos = harissa.graphics.build_pos(inter)
+    harissa.graphics.plot_network(inter, pos, scale=2)
+    print('done')
+
+# simulate command
 def simulate(args):
     print('simulate')
     return
@@ -50,7 +58,8 @@ def simulate(args):
 
 
 def main():
-    main_parser = argparse.ArgumentParser(
+    # Main parser
+    main_parser = ap.ArgumentParser(
         description='Tools for mechanistic gene network inference '
                     'from single-cell data'
     )
@@ -60,13 +69,16 @@ def main():
         action='version', 
         version=harissa.__version__)
     
+    # Sub commands infer and simulate
     subparsers = main_parser.add_subparsers(
         title='commands',
         help='command help',
         required=True
     )
-
     infer_parser = subparsers.add_parser('infer', help='infer help')
+    simulate_parser = subparsers.add_parser('simulate', help='simulate help')
+    
+    # Infer parser
     infer_parser.add_argument('data_path', type=Path, help="path to data file")
     infer_parser.add_argument(
         '--cut-off',
@@ -79,19 +91,22 @@ def main():
     )
     infer_parser.set_defaults(run=infer)
     
+    # Inference methods hartree (cardamom TODO)
     infer_subparser = infer_parser.add_subparsers(
         title='inference methods', 
         required= False
     )
     hartree_parser = infer_subparser.add_parser('hartree')
+
+    # Hartree parser
     hartree_parser.add_argument('-p', '--penalization', type=float)
     hartree_parser.add_argument('-t', '--tolerance', type=float)
     hartree_parser.add_argument('-n', '--max-iteration', type=int)
     hartree_parser.add_argument('-v', '--verbose', action='store_true')
-    hartree_parser.add_argument('--use-numba', action=argparse.BooleanOptionalAction)
+    hartree_parser.add_argument('--use-numba', action=ap.BooleanOptionalAction)
     hartree_parser.set_defaults(create_inference=create_hartree)
 
-    simulate_parser = subparsers.add_parser('simulate', help='simulate help')
+    # Simulate parser
     # simulate_parser.add_argument(
     #     'filename', 
     #     type=argparse.FileType(),
@@ -102,21 +117,26 @@ def main():
     )
     simulate_parser.set_defaults(run=simulate)
     
+    # Simulation methods bursty and ode
     simulate_subparser = simulate_parser.add_subparsers(
         title='simulation methods', 
         required=False
     )
     bursty_parser = simulate_subparser.add_parser('bursty')
-    bursty_parser.add_argument('--thin-adapt', action=argparse.BooleanOptionalAction)
+    ode_parser = simulate_subparser.add_parser('ode')
+
+    # Bursty parser
+    bursty_parser.add_argument('--thin-adapt', action=ap.BooleanOptionalAction)
     bursty_parser.add_argument('-v', '--verbose', action='store_true')
     bursty_parser.add_argument('--use-numba', action='store_true')
     bursty_parser.set_defaults(create_simulation=create_bursty)
     
-    ode_parser = simulate_subparser.add_parser('ode')
+    # Ode parser
     ode_parser.add_argument('-v', '--verbose', action='store_true')
     ode_parser.add_argument('--use-numba', action='store_true')
     ode_parser.set_defaults(create_simulation=create_ode)
     
+    # parse sys.argv and run the command before exiting
     args = main_parser.parse_args()
     args.run(args)
     main_parser.exit()
