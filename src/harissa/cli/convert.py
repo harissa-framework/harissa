c7ba2422dda24e98a34cee493fe03508dd69044a
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-01-29 19:37:04 +0100

    add convert and dataset command

---
+++ src/harissa/cli/convert.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from harissa.utils.npz_io import convert 
+
+def add_subcommand(main_subparsers):
+    parser = main_subparsers.add_parser(
+        'convert',
+        help='convert help'
+    )
+
+    parser.add_argument(
+        'path', 
+        type=Path,
+        help='path to convert. It is a .npz file or a directory.'
+    )
+
+    parser.set_defaults(run=convert)
