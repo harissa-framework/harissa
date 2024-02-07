fa9d8d33b601652ac85c106a14857996ec2b6d14
Ulysse Herbach <35532736+ulysseherbach@users.noreply.github.com>
2024-02-07 16:28:31 +0100

    Update dataset.py

--- src/harissa/core/dataset.py
+++ src/harissa/core/dataset.py
@@ -8,9 +8,12 @@ class Dataset:
 
     def __init__(self, 
                  time_points: np.ndarray, 
-                 count_matrix: np.ndarray) -> None:
+                 count_matrix: np.ndarray,
+                 gene_names=None) -> None:
+
         if not (time_points.ndim == 1 and time_points.dtype == np.float_):
             raise TypeError('time_points must be a float 1D ndarray.')
+
         if not (count_matrix.ndim == 2 and count_matrix.dtype == np.uint):
             raise TypeError('count_matrix must be an uint 2D ndarray.')
 
@@ -20,9 +23,10 @@ class Dataset:
                 ' than the rows of count_matrix.' 
                 f'({time_points.shape[0]} != {count_matrix.shape[0]})'
             )
+
         if count_matrix.shape[1] <= 1:
             raise TypeError('count_matrix must have at least 2 columns.')
 
         object.__setattr__(self, 'time_points', time_points)
         object.__setattr__(self, 'count_matrix', count_matrix)
-        
+        object.__setattr__(self, 'gene_names', gene_names)
