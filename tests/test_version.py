e49f6ad7fc0d4a331b66ad4d009dd35391e8aea6
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-03-18 16:59:23 +0100

    rename fixture

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,22 +5,22 @@ from importlib.metadata import version, PackageNotFoundError
 import harissa
 
 @pytest.fixture
-def simulate_import_without_installing():
+def __version__():
     sys_path = sys.path
     sys.path = [harissa.__path__[0]]
 
-    yield
+    from __init__ import __version__
+
+    yield __version__
 
     sys.path = sys_path
 
 def test_version():
     assert harissa.__version__ == version('harissa')
 
-def test_unknown_version(simulate_import_without_installing):
+def test_unknown_version(__version__):
     with pytest.raises(PackageNotFoundError):
         version('harissa')
-
-    from __init__ import __version__
 
     assert __version__ == 'unknown version'
 
