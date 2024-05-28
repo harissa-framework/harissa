5b1302e058adb8f9f04d01849a6d300c5811af7a
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-05-28 17:40:10 +0200

    add pearson inference

--- src/harissa/inference/__init__.py
+++ src/harissa/inference/__init__.py
@@ -6,8 +6,9 @@ Inference of the network model.
 """
 from harissa.inference.hartree import Hartree
 from harissa.inference.cardamom import Cardamom
+from harissa.inference.pearson import Pearson
 
 # Default inference method
 default_inference = Hartree
 
-__all__ = ['Hartree', 'Cardamom']
+__all__ = ['Hartree', 'Cardamom', 'Pearson']
---
+++ src/harissa/inference/pearson.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy import stats
+
+from harissa.core.inference import Inference, NetworkParameter, Dataset
+
+class Pearson(Inference):
+    def run(self,
+        dataset: Dataset,
+        param: NetworkParameter
+    ) -> Inference.Result:
+        n_gene_stim = dataset.count_matrix.shape[1]
+        score = np.zeros((n_gene_stim, n_gene_stim))
+
+        for i in range(n_gene_stim):
+            for j in range(n_gene_stim):
+                score[i, j] = stats.pearsonr(
+                    dataset.count_matrix[:, i],
+                    dataset.count_matrix[:, j]
+                )[0]
+
+        param.interaction[:] = score
+
+        return self.Result(param)
