547640affadbaea268402185d013ff61d0ac11c7
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-07-05 20:52:14 +0200

    add pydata-sphinx-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,35 +3,58 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
 import re 
 import sys
 from importlib.metadata import version as get_version, PackageNotFoundError
 from pathlib import Path
 from shutil import copytree, rmtree, ignore_patterns
+import json
 
 from sphinxcontrib.collections.drivers import Driver
 from sphinxcontrib.collections.api import register_driver
 
-project_dir = Path(__file__).parent.parent.parent
-tmp_dir = project_dir
 
-cmd_name = Path(os.environ['_']).name
-is_multi_version_sub_process = (
-    cmd_name == 'sphinx-multiversion' 
-    and cmd_name != Path(sys.argv[0]).name
+# -------------- Utility functions and class -----------------------
+
+# Convention for version number https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers
+# [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
+VERSION_PATTERN = re.compile(
+    r'^(\d+!)?\d+(\.\d+)*((a|b|rc)\d+)?(\.post\d+)?(\.dev\d+)?$'
 )
+switcher_filename = 'switcher.json' 
 
+with open(Path(__file__).parent / switcher_filename) as fp:
+    switcher_data = list(filter(
+        lambda data: re.match(VERSION_PATTERN, data['version']), 
+        json.load(fp)
+    ))
 
-if is_multi_version_sub_process:
-    tmp_dir = Path(sys.argv[-2]).parent.parent
-    sys.path[0] =  str(tmp_dir / 'src')
-    def root_index_content(redirect_version):
-        v_index = f'{redirect_version}/index.html' 
-        relative_url = f'./{v_index}'
-        absolute_url = f'harissa-framework.github.io/harissa/{v_index}'
+def tag_whitelist():
+    whitelist = r'^'
+    if switcher_data:
+        whitelist += r'('
+        for i, data in enumerate(switcher_data):
+            version = data['version'].replace(r'.', r'\.')
+            if i > 0:
+                whitelist += rf'|{version}'
+            else:
+                whitelist += rf'{version}'
+        whitelist += r')'
 
-        return f'''<!DOCTYPE html>
+    whitelist += r'$'
+
+    return whitelist
+
+def to_sem_ver(version):
+    semver = re.sub(r'(\d+)(a|b|rc)(\d+)', r'\g<1>-pre.\g<3>', version)
+    return re.sub(r'\.dev(\d+)', r'+build.\g<1>', semver)
+
+def root_index_content(project, redirect_version):
+    v_index = f'{redirect_version}/index.html' 
+    relative_url = f'./{v_index}'
+    absolute_url = f'/{project}/{v_index}'
+
+    return f'''<!DOCTYPE html>
 <html>
     <head>
         <title>Redirecting to version {redirect_version}</title>
@@ -41,9 +64,50 @@ if is_multi_version_sub_process:
     </head>
 </html>
 '''
+
+class CopyFolderOnly(Driver):
+    def copy_only(self, path, names):
+        patterns = self.config.get('only', [])
+        return set(names) - ignore_patterns(*patterns)(path, names)
     
-use_nbsphinx_link = (tmp_dir / 'docs' / 'source' / 'notebooks').is_dir()
-print(f'{use_nbsphinx_link=}')
+    def run(self):
+        self.info(
+            (f'Copy folder {self.config["source"]} '
+            f'into {self.config["target"]} ...')
+        )
+
+        if not Path(self.config['source']).exists():
+            self.error(f'Source {self.config["source"]} does not exist')
+            return
+
+        try:
+            copytree(
+                self.config['source'], 
+                self.config['target'], 
+                ignore=self.copy_only,
+                dirs_exist_ok=True # for sphinx-autobuild
+            )
+        except IOError as e:
+            self.error("Problems during copying folder.", e)
+
+    def clean(self):
+        try:
+            rmtree(self.config['target'])
+            self.info(f'Folder deleted: {self.config["target"]}')
+        except FileNotFoundError:
+            pass  # Already cleaned? I'm okay with it.
+        except IOError as e:
+            error_msg = 'Problems during cleaning for collection '
+            error_msg = error_msg + self.config['name']
+            self.error(error_msg, e)
+
+def reset_conf_dir(conf_dir):
+    def wrapper(app, config):
+        app.confdir = conf_dir
+
+    return wrapper
+
+main_branch = 'pydata-sphinx-theme'
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -51,17 +115,13 @@ print(f'{use_nbsphinx_link=}')
 project = 'Harissa'
 copyright = '2023, Ulysse Herbach'
 author = 'Ulysse Herbach'
-# harissa needs to be installed (at least in editable mode)
-import harissa
 
-if hasattr(harissa, '__version__'):
-    version = harissa.__version__
-else:
-    try:
-        version = get_version(project.lower())
-    except PackageNotFoundError:
-        version = 'unknown version'
+try:
+    main_version = get_version(project.lower())
+except PackageNotFoundError:
+    raise RuntimeError('harissa must be installed for the autodoc to work.')
 
+version = main_version
 release = version
 
 # -- General configuration ---------------------------------------------------
@@ -69,95 +129,15 @@ release = version
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     # 'sphinx.ext.coverage',
     'sphinx_multiversion',
-    # 'sphinx_gallery.gen_gallery',
     'sphinx_copybutton',
-    # 'myst-nb'
 ]
-
-myst_enable_extensions = [
-    # 'amsmath',
-    'dollarmath',
-    'colon_fence',
-    'strikethrough',
-    'tasklist',
-]
-
-nb_number_source_lines = True
-
-# myst_heading_anchors = 3
-
-if use_nbsphinx_link:
-    extensions.append('nbsphinx')
-    extensions.append('nbsphinx_link')
-    extensions.append('sphinx_gallery.load_style')
-else:
-    extensions.append('sphinxcontrib.collections')
-    extensions.append('myst_nb')
-
-    # -- Options for sphinx collections output -------------------------------
-    # https://sphinx-collections.readthedocs.io/en/latest/
-
-    class CopyFolderOnly(Driver):
-        def copy_only(self, path, names):
-            patterns = self.config.get('only', [])
-            return set(names) - ignore_patterns(*patterns)(path, names)
-        
-        def run(self):
-            # override it to be able to copy in the tmp_dir (multiversion)
-            # rel_dir = Path(self.config['target']).relative_to(project_dir)
-            # self.config['target'] = str(tmp_dir / rel_dir)
-
-            self.info(f'is sub process: {is_multi_version_sub_process}')
-
-            self.info(
-                (f'Copy folder {self.config["source"]} '
-                f'into {self.config["target"]} ...')
-            )
-
-            if not Path(self.config['source']).exists():
-                self.error(f'Source {self.config["source"]} does not exist')
-                return
-
-            try:
-                copytree(
-                    self.config['source'], 
-                    self.config['target'], 
-                    ignore=self.copy_only,
-                    dirs_exist_ok=True # for sphinx-autobuild
-                )
-            except IOError as e:
-                self.error("Problems during copying folder.", e)
-
-        def clean(self):
-            try:
-                rmtree(self.config['target'])
-                self.info(f'Folder deleted: {self.config["target"]}')
-            except FileNotFoundError:
-                pass  # Already cleaned? I'm okay with it.
-            except IOError as e:
-                self.error(f'Problems during cleaning for collection {self.config["name"]}', e)
-
-    register_driver('copy_folder_only', CopyFolderOnly)
-
-    # coverage_show_missing_items = True
-    # coverage_statistics_to_stdout = True
-
-    collections = {
-        'notebooks' : {
-            'driver': 'copy_folder_only',
-            'source': str(tmp_dir / 'notebooks'),
-            # 'target': str(tmp_dir/'docs'/'source'/'_collections'/'notebooks'),
-            'only': ['*.ipynb'],
-        }
-    }
-    collections_target = str(tmp_dir/'docs'/'source'/'_collections')
 
 # -- Options for autodoc output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
@@ -170,46 +150,27 @@ autosummary_generate = True
 autosummary_ignore_module_all = False
 # autosummary_imported_members = True
 
-# sphinx_gallery_conf = {
-#     'examples_dirs': '../../notebooks',   # path to your example scripts
-#     'gallery_dirs': 'notebooks',  # path to where to save gallery generated output
-# }
-
-# source_suffix = {
-#     '.rst': 'restructuredtext',
-#     '.txt': 'markdown',
-#     '.md': 'markdown',
-#     '.ipynb': 'markdown',
-# }
-
 templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-if is_multi_version_sub_process:
-    html_title = f'{project} documentation'
-if 'HTML_TITLE' in os.environ:
-    html_title = os.environ['HTML_TITLE']
-
-html_theme = 'furo'
+html_baseurl = f'https://harissa-framework.github.io/{project.lower()}/'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-# html_theme_options = {
-#     "announcement": "<em>Under Construction !</em>",
-# }
-html_sidebars = {
-    '**': [
-        'sidebar/brand.html',
-        'sidebar/versioning.html',
-        'sidebar/search.html',
-        'sidebar/scroll-start.html',
-        'sidebar/navigation.html',
-        # 'sidebar/ethical-ads.html',
-        'sidebar/scroll-end.html',
-        # 'sidebar/variant-selector.html',
-    ],
+html_theme_options = {
+    'icon_links': [
+        {
+            'name': 'GitHub',
+            'url': f'https://github.com/harissa-framework/{project.lower()}',
+            'icon': 'fa-brands fa-github',
+            'type': 'fontawesome',
+        }
+   ],
+   'pygments_light_style': 'default',
+   'pygments_dark_style': 'material',
 }
 html_copy_source = False
 
@@ -223,103 +184,166 @@ nbsphinx_execute = 'never'
 
 nb_execution_mode = 'off'
 
+# -- Options for myst-nb ----------------------------------------------------
+
+nb_number_source_lines = True
+
+# myst_heading_anchors = 3
+
+myst_enable_extensions = [
+    # 'amsmath',
+    'dollarmath',
+    'colon_fence',
+    'strikethrough',
+    'tasklist',
+]
+
 #-- Options for Napoleon ----------------------------------------------------
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
-
 # -- Options for sphinx-copybutton ------------------------------------------
 # https://sphinx-copybutton.readthedocs.io/en/latest
-copybutton_exclude = '.linenos, .gp, .go, .o'
-# copybutton_prompt_text = r"[$!]\s*"
-# copybutton_prompt_is_regexp = True
-
-# Convention for version number https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers
-# [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
-pattern = re.compile(
-    r'^v(\d+!)?\d+(\.\d+)*((a|b|rc)\d+)?(\.post\d+)?(\.dev\d+)?$'
-)
-active_versions = [
-    # 'v3.0.7',
-    'v3.0.8',
-    # 'v4.0.0',
-]
-active_versions = tuple(
-    filter(lambda v: re.match(pattern, v), active_versions)
-)
+copybutton_exclude = '.linenos, .gp, .go'
+copybutton_prompt_text = r'[$!]\s*'
+copybutton_prompt_is_regexp = True
 
 # -- Options for sphinx-multiversion
 # https://holzhaus.github.io/sphinx-multiversion/master/index.html
 
-tag_whitelist = r'^'
-if active_versions:
-    tag_whitelist += r'('
-    for i, active_version in enumerate(active_versions):
-        active_version_escaped = active_version.replace(r'.', r'\.')
-        if i > 0:
-            tag_whitelist += rf'|{active_version_escaped}'
-        else:
-            tag_whitelist += rf'{active_version_escaped}'
-    tag_whitelist += r')'
-
-    # Choose here the version latest version
-    smv_latest_version = active_versions[-1]
-
-    if is_multi_version_sub_process:
-        # Choose here the version to redirect when landing on the root index
-        redirect_version = smv_latest_version
-        def generate_root_index(app, exception):
-            if exception is not None:
-                return
-            
-            root_index = os.path.join(app.outdir, '..' , 'index.html')
-                
-            print('\033[1mBuilding root index.html\033[0m')
-
-            with open(root_index, 'w') as file:
-                file.write(root_index_content(redirect_version))   
-
-            print(f'{root_index} generated.\n' 
-                f'It redirects to version {redirect_version}')
-
-tag_whitelist += r'$'
-
-smv_tag_whitelist = tag_whitelist
-smv_branch_whitelist = r'^main$'
+smv_tag_whitelist = tag_whitelist()
+smv_branch_whitelist = rf'^{main_branch}$'
 smv_remote_whitelist = r'^$'
 smv_released_pattern = r'^tags/v\d+(\.\d+)*$'
 
+def setup_collections(app, config):
+    doc_src_dir = Path(app.srcdir)
+    # -- Options for sphinx collections output -------------------------------
+    # https://sphinx-collections.readthedocs.io/en/latest/
+    config.collections = {
+        'notebooks' : {
+            'driver': 'copy_folder_only',
+            'source': str(doc_src_dir.parent.parent / 'notebooks'),
+            'only': ['*.ipynb'],
+        }
+    }
+    config.collections_target = str(doc_src_dir)
+    # hack for sphinx collections and multiversion
+    app.confdir = app.srcdir
+
+def setup_multi_version(app, config):
+    current_version = config.smv_current_version
+    if current_version:
+        sys.path[0] = str(Path(app.srcdir).parent.parent / 'src')
+        output_root = Path(app.outdir).parent
+
+        redirect_version = main_branch
+        main_semver = to_sem_ver(main_version)
+
+        if current_version == main_branch:
+            app.outdir = output_root / 'latest'
+            current_semver = main_semver
+        else:
+            current_semver = to_sem_ver(current_version[1:])
+
+        config.version = current_semver
+        config.release = config.version
+
+        config.html_title = f'{project} documentation'
+        config.html_theme_options['switcher'] = {
+            'json_url' : switcher_filename, 
+            'version_match': current_semver
+        }
+        config.html_theme_options['show_version_warning_banner'] = True
+        config.html_theme_options['navbar_start'] = [
+            'navbar-logo', 
+            'version-switcher'
+        ]
+
+        for data in switcher_data:
+            if data.get('preferred', False):
+                redirect_version = data['version']
+                break
+        
+        project_lower = project.lower()
+        for data in switcher_data:
+            data['url'] = f"/{project_lower}/{data['version']}/"
+            if 'name' not in data:
+                data['name'] = data['version']
+            data['version'] = to_sem_ver(data['version'][1:])
+
+        main_branch_data = {
+            'name': f'v{version}', 
+            'version': main_semver,
+            'url': f'/{project_lower}/latest/'
+        }
+
+        if redirect_version == main_branch:
+            redirect_version = 'latest'
+            main_branch_data['preferred'] = True
+
+        data = [main_branch_data] + switcher_data
+        with open(Path(app.srcdir) / switcher_filename, 'w') as fp:
+            json.dump(data, fp, indent=4)
+
+        if current_version == redirect_version:
+            print('\033[1mBuilding Switcher.json\033[0m')
+            with open(output_root / switcher_filename, 'w') as fp:
+                json.dump(data, fp, indent=4)
+            print('Switcher generated.')
+
+            print('\033[1mBuilding root index.html\033[0m')
+            with open(output_root / 'index.html', 'w') as fp:
+                fp.write(root_index_content(project_lower, redirect_version))
+            print(f'Root index generated.\n' 
+            f'It redirects to version {redirect_version}')
+    
 def clean_up(app, exception):
     if exception is not None:
-        return
+        return 
+    print(f'\033[1m{sys.modules["harissa"]=}\033[0m')
+    
+    output = Path(app.outdir)
+    current_version = app.config.smv_current_version
+    
+    if current_version:
+        if current_version == main_branch:
+            print(f'\033[1mCleaning {current_version}\033[0m')
+            rmtree(output.parent / current_version)
 
-    myst_nb_jupyter_execute_dir = Path(app.outdir).parent / 'jupyter_execute'
+        print('\033[1mCleaning extras\033[0m')
+        for subpath in ['.doctrees', 'doctrees', 'objects.inv', '.buildinfo']:
+            path = output / subpath
+            if path.is_dir():
+                rmtree(path)
+            elif path.is_file():
+                path.unlink()
+
+    myst_nb_jupyter_execute_dir = output.parent / 'jupyter_execute'
     if myst_nb_jupyter_execute_dir.is_dir():
         print('\033[1mCleaning jupyter_execute\033[0m')
         rmtree(myst_nb_jupyter_execute_dir)
+    
 
-    collections_dir = Path(app.confdir) / '_collections'
-    if collections_dir.is_dir():
-        print('\033[1mCleaning _collections\033[0m')
-        rmtree(collections_dir)
+def update_json_url(app, pagename, templatename, context, doctree):
+    if app.config.smv_current_version:
+        context['theme_switcher']['json_url'] = (
+            f'/{project.lower()}/{switcher_filename}'
+        )
 
 def setup(app):
-    if is_multi_version_sub_process:
-        current_version = Path(app.outdir).name
-        # hack for sphinx collections and multiversion
-        if not use_nbsphinx_link:
-            app.confdir = tmp_dir /'docs'/'source'
-        
-        # fallback redirection
-        if current_version == active_versions[0]:
-            root_index = os.path.join(app.outdir, '..' , 'index.html')
-            with open(root_index, 'w') as file:
-                file.write(root_index_content(active_versions[0]))
+    if (Path(app.srcdir) / 'notebooks').is_dir():
+        app.setup_extension('nbsphinx')
+        app.setup_extension('sphinx_gallery.load_style')
+    else:
+        conf_dir = app.confdir
 
-        for active_version in active_versions:
-            if (redirect_version == active_version 
-                and active_version == current_version):
-                app.connect('build-finished', generate_root_index)
-                break
+        register_driver('copy_folder_only', CopyFolderOnly)
+        app.connect('config-inited', setup_collections)
+        app.setup_extension('sphinxcontrib.collections')
+        app.connect('config-inited', reset_conf_dir(conf_dir))
+        app.setup_extension('myst_nb')
 
+    app.connect('config-inited', setup_multi_version)
+    app.connect('html-page-context', update_json_url)
     app.connect('build-finished', clean_up)
--- docs/source/notebooks.md
+++ docs/source/notebooks.md
@@ -8,6 +8,6 @@
 :::{toctree}
 :glob:
 
-_collections/notebooks/*
+notebooks/*
 :::
  
