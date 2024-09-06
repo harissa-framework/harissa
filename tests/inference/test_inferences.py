1efe8b8b749149eca3ebf3674c8dd2585b26e496
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-09-06 18:08:52 +0200

    add test json

--- tests/inference/test_inferences.py
+++ tests/inference/test_inferences.py
@@ -1,6 +1,7 @@
 import sys
 from inspect import getmembers, isclass
 import numpy as np
+from json import load
 
 from harissa.core import Inference, NetworkParameter, Dataset
 import harissa.inference
@@ -14,6 +15,7 @@ def _create_test_group(cls):
             inf = cls()
             assert hasattr(inf, 'run')
             assert hasattr(inf, 'directed')
+            assert hasattr(inf, '_serialize')
 
         def test_run_output(self):
             inf = cls()
@@ -32,11 +34,41 @@ def _create_test_group(cls):
             n_genes_stim = data.count_matrix.shape[1]
 
             assert isinstance(res, Inference.Result)
-            
+
             assert hasattr(res, 'parameter')
             assert isinstance(res.parameter, NetworkParameter)
 
             assert res.parameter.n_genes_stim == n_genes_stim
+
+        def test_json(self, tmp_path):
+            inf = cls()
+            inf_file = tmp_path / f'{cls.__name__.lower()}.json'
+            inf.save_json(inf_file)
+
+            assert inf_file.is_file()
+            assert inf_file.suffix == '.json'
+
+            with open(inf_file, 'r') as fp:
+                inf_info = load(fp)
+
+            assert 'classname' in inf_info
+            assert 'module' in inf_info
+            assert 'kwargs' in inf_info
+
+            assert inf_info['classname'] == cls.__name__
+            assert inf_info['module'] == inf.__module__
+            assert isinstance(inf_info['kwargs'], dict)
+
+            inf_loaded = cls.load_json(inf_file)
+            inf_loaded2 = Inference.load_json(inf_file)
+
+            assert type(inf_loaded) is cls
+            assert type(inf_loaded) is type(inf)
+            assert type(inf_loaded) is type(inf_loaded2)
+
+            assert inf_loaded is not inf
+            assert inf_loaded is not inf_loaded2
+
 
     return (f'{Test.__name__}{cls.__name__}', Test)
 
