0a5acb7b3c43d552f0683ecb3f372d6072ee56e6
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-09-13 17:34:20 +0200

    add tests

--- tests/core/test_inference.py
+++ tests/core/test_inference.py
@@ -1,11 +1,36 @@
 import pytest
 import numpy as np
+from json import dump
+import sys
 from harissa.core import Inference, Dataset, NetworkParameter
 from harissa.benchmark.generators.networks import bn8
 
 @pytest.fixture(scope='module')
 def network_parameter():
     return bn8()
+
+@pytest.fixture(scope='module')
+def inf_error_json(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp('inference')
+    sys_path = sys.path
+    sys.path.append(str(tmp_path))
+
+    inf_path = tmp_path / 'my_inference.py'
+    path = tmp_path / 'inf_error.json'
+
+    with open(inf_path, 'w') as fp:
+        fp.write('class MyInference:\n\tpass')
+
+    with open(path, 'w') as fp:
+        dump({
+            'classname': 'MyInference',
+            'module' : f'{inf_path.stem}',
+            'kwargs': {}
+        }, fp)
+
+    yield path
+
+    sys.path = sys_path
 
 @pytest.fixture(scope='module')
 def res_npz(tmp_path_factory, network_parameter):
@@ -21,6 +46,15 @@ def res_txt(tmp_path_factory, network_parameter):
 
     return path
 
+@pytest.fixture(scope='module')
+def res_json(tmp_path_factory, network_parameter):
+    path = tmp_path_factory.mktemp('inference_result') / 'result.json'
+    network_parameter.save_json(path)
+
+    return path
+
+class MyInference:
+    pass
 
 class InferenceMissing(Inference):
     pass
@@ -60,6 +94,11 @@ class TestInference:
         with pytest.raises(NotImplementedError):
             inf.directed
 
+    def test_load_not_Inference(self, inf_error_json):
+
+        with pytest.raises(RuntimeError):
+            Inference.load_json(inf_error_json)
+
 class TestInferenceResult:
     def test_init(self):
         param = NetworkParameter(1)
@@ -86,15 +125,20 @@ class TestInferenceResult:
 
         assert res.parameter == network_parameter
 
+    def test_load_json(self, res_json, network_parameter):
+        res = Inference.Result.load_json(res_json)
+
+        assert res.parameter == network_parameter
+
     def test_save(self, tmp_path):
-        res = Inference.Result(NetworkParameter(1))
+        res = Inference.Result(NetworkParameter(2))
 
         path = res.save(tmp_path / 'res.npz')
         param = NetworkParameter.load(path)
 
         assert res.parameter == param
 
-        res = Inference.Result(NetworkParameter(1), foo='bar')
+        res = Inference.Result(NetworkParameter(2), foo='bar')
 
         path = res.save(tmp_path / 'res.npz', save_extra=True)
         param = NetworkParameter.load(path)
@@ -102,16 +146,31 @@ class TestInferenceResult:
         assert res.parameter == param
 
     def test_save_txt(self, tmp_path):
-        res = Inference.Result(NetworkParameter(1))
+        res = Inference.Result(NetworkParameter(2))
 
         path = res.save_txt(tmp_path / 'res')
         param = NetworkParameter.load_txt(path)
 
         assert res.parameter == param
 
-        res = Inference.Result(NetworkParameter(1), foo='bar')
+        res = Inference.Result(NetworkParameter(2), foo='bar')
 
         path = res.save_txt(tmp_path / 'res', save_extra=True)
         param = NetworkParameter.load_txt(path)
+
+        assert res.parameter == param
+
+    def test_save_json(self, tmp_path):
+        res = Inference.Result(NetworkParameter(2))
+
+        path = res.save_json(tmp_path / 'res.json')
+        param = NetworkParameter.load_json(path)
+
+        assert res.parameter == param
+
+        res = Inference.Result(NetworkParameter(2), foo='bar')
+
+        path = res.save_json(tmp_path / 'res.json', save_extra=True)
+        param = NetworkParameter.load_json(path)
 
         assert res.parameter == param
